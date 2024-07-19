a6b6ccf33532baa2e68a3c037b068888db280ad2
Shreyaskr8 <skrravi08@gmail.com>
2024-07-19 23:41:10 +0530

    Added svgs for edit and delete buttons, redesigned todo card to flex.
    Removed '+' from 'New Project' btn
    
    background color changes, changed main-content
    from grid to flex to redesign todo card.

--- src/components/display.js
+++ src/components/display.js
@@ -3,7 +3,14 @@ import state from "./state";
 
 const mainContentDiv = document.querySelector(".main-content");
 const projectsDiv = document.querySelector(".projects");
+let selectedProjectDiv = null;
 const dialog = document.querySelector("dialog");
+const DELETE_ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0 0 24 24">
+    <path d="M 10 2 L 9 3 L 5 3 C 4.448 3 4 3.448 4 4 C 4 4.552 4.448 5 5 5 L 7 5 L 17 5 L 19 5 C 19.552 5 20 4.552 20 4 C 20 3.448 19.552 3 19 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.105 5.895 22 7 22 L 17 22 C 18.105 22 19 21.105 19 20 L 19 7 L 5 7 z"></path>
+    </svg>`;
+const EDIT_ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0 0 24 24">
+    <path d="M 18.414062 2 C 18.158188 2 17.902031 2.0974687 17.707031 2.2929688 L 16 4 L 20 8 L 21.707031 6.2929688 C 22.098031 5.9019687 22.098031 5.2689063 21.707031 4.8789062 L 19.121094 2.2929688 C 18.925594 2.0974687 18.669937 2 18.414062 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z"></path>
+    </svg>`;
 
 function addEventListenersForBtn(btnEle) {
     btnEle.addEventListener("click", () => {
@@ -14,24 +21,30 @@ function addEventListenersForBtn(btnEle) {
 function addDefaultProject(projectName) {
     mainContentDiv.replaceChildren();
     const projectDiv = document.createElement("div");
-    projectDiv.className = "project";
+    projectDiv.className = "project default-project";
     projectsDiv.appendChild(projectDiv);
-
-    const projectBtn = document.createElement("button");
-    projectBtn.className = "project-btn";
-    projectBtn.innerText = projectName;
-    projectBtn.addEventListener("click", () => {
+    projectDiv.addEventListener("click", () => {
+        if(selectedProjectDiv) {
+            selectedProjectDiv.classList.remove("highlight-project")
+        }
+        projectDiv.classList.add("highlight-project");
+        selectedProjectDiv = projectDiv;
         let loadedProject = storage.loadProjectFromLocalStorage(projectName);
         state.setCurrentProject(loadedProject);
         displayProject(loadedProject);
     });
 
+
+    const projectNameEle = document.createElement("p");
+    projectNameEle.className = "project-name";
+    projectNameEle.innerText = projectName;
+
     const createTodoBtn = document.createElement("button");
-    createTodoBtn.className = "create-todo-btn";
+    createTodoBtn.className = "create-todo-btn-default";
     createTodoBtn.innerText = "+";
     addEventListenersForBtn(createTodoBtn);
 
-    projectDiv.appendChild(projectBtn);
+    projectDiv.appendChild(projectNameEle);
     projectDiv.appendChild(createTodoBtn);
 }
 
@@ -40,15 +53,20 @@ function addNewProject(projectName) {
     const projectDiv = document.createElement("div");
     projectDiv.className = "project";
     projectsDiv.appendChild(projectDiv);
-
-    const projectBtn = document.createElement("button");
-    projectBtn.className = "project-btn";
-    projectBtn.innerText = projectName;
-    projectBtn.addEventListener("click", () => {
+    projectDiv.addEventListener("click", () => {
+        if(selectedProjectDiv) {
+            selectedProjectDiv.classList.remove("highlight-project")
+        }
+        projectDiv.classList.add("highlight-project");
+        selectedProjectDiv = projectDiv;
         let loadedProject = storage.loadProjectFromLocalStorage(projectName);
         state.setCurrentProject(loadedProject);
         displayProject(loadedProject);
     });
+    
+    const projectNameEle = document.createElement("p");
+    projectNameEle.className = "project-name";
+    projectNameEle.innerText = projectName;
 
     const createTodoBtn = document.createElement("button");
     createTodoBtn.className = "create-todo-btn";
@@ -57,14 +75,14 @@ function addNewProject(projectName) {
 
     const deleteProjectBtn = document.createElement("button");
     deleteProjectBtn.className = "delete-project-btn";
-    deleteProjectBtn.innerText = "X";
+    deleteProjectBtn.innerHTML = DELETE_ICON_SVG;
     deleteProjectBtn.addEventListener("click", () => {
         projectsDiv.removeChild(projectDiv);
         mainContentDiv.replaceChildren();
         storage.deleteProjectFromLocalStorage(projectName);
     });
 
-    projectDiv.appendChild(projectBtn);
+    projectDiv.appendChild(projectNameEle);
     projectDiv.appendChild(createTodoBtn);
     projectDiv.appendChild(deleteProjectBtn);
 
@@ -97,10 +115,17 @@ function displayTodos(todos) {
         const todoPriority = document.createElement("p");
         todoPriority.className = "priority";
         todoPriority.innerText = todo.priority;
+        
+        const editTodoBtn = document.createElement("button");
+        editTodoBtn.className = "edit-todo-btn";
+        editTodoBtn.innerHTML = EDIT_ICON_SVG;
+        editTodoBtn.addEventListener("click", () => {
+            // bring up the dialog filled with current todo card info.
+        })
 
         const deleteTodoBtn = document.createElement("button");
         deleteTodoBtn.className = "delete-todo-btn";
-        deleteTodoBtn.innerText = "del";
+        deleteTodoBtn.innerHTML = DELETE_ICON_SVG;
         deleteTodoBtn.addEventListener("click", () => {
             deleteTodo(todoCard, todo);
         })
@@ -109,6 +134,7 @@ function displayTodos(todos) {
         todoCard.appendChild(todoDesc);
         todoCard.appendChild(todoDueDate);
         todoCard.appendChild(todoPriority);
+        todoCard.appendChild(editTodoBtn);
         todoCard.appendChild(deleteTodoBtn);
         
     });
