49bd83f2588a684d4a2eb9c6dd4dc9f7c2ac93e2
Shreyaskr8 <skrravi08@gmail.com>
2024-07-21 00:07:10 +0530

    add feat. show description on hover

--- src/components/display.js
+++ src/components/display.js
@@ -4,6 +4,7 @@ import state from "./state";
 const todosContentDiv = document.querySelector(".todos-content");
 const projectsDiv = document.querySelector(".projects");
 let selectedProjectDiv = null;
+let currentDescDisplayed = null;
 const dialog = document.querySelector("dialog");
 const DELETE_ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0 0 24 24">
     <path d="M 10 2 L 9 3 L 5 3 C 4.448 3 4 3.448 4 4 C 4 4.552 4.448 5 5 5 L 7 5 L 17 5 L 19 5 C 19.552 5 20 4.552 20 4 C 20 3.448 19.552 3 19 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.105 5.895 22 7 22 L 17 22 C 18.105 22 19 21.105 19 20 L 19 7 L 5 7 z"></path>
@@ -106,6 +107,13 @@ function displayTodos(todos) {
         const todoDesc = document.createElement("p");
         todoDesc.className = "desc";
         todoDesc.innerText = todo.description;
+        todoDesc.addEventListener("mouseover", () => {
+            currentDescDisplayed = showTodoDescription(todoCard, todoDesc.innerText);
+        });
+
+        todoDesc.addEventListener("mouseout", () => {
+            hideTodoDescription(todoCard, currentDescDisplayed);
+        })
         
         const todoDueDate = document.createElement("p");
         todoDueDate.className = "due-date";
@@ -155,6 +163,24 @@ function addTodoSeparator() {
     const borderAfterTodoCard = document.createElement("p");
     borderAfterTodoCard.style.borderBottom = "1px solid rgb(153, 153, 153)";
     todosContentDiv.appendChild(borderAfterTodoCard);
+}
+
+function showTodoDescription(todoCard, todoDescription) {
+    const descDiv = document.createElement("div");
+    descDiv.className = "display-desc";
+    const descContent = document.createElement("p");
+    descContent.innerText = todoDescription;
+    descDiv.appendChild(descContent);
+    todoCard.appendChild(descDiv);
+    return descDiv;
+}
+
+function hideTodoDescription(todoCard, descDiv) {
+    if(descDiv == null) {
+        return;
+    }
+    todoCard.removeChild(descDiv);
+    descDiv = null;
 }
 
 export default {
