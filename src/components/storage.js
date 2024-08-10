7dc66ebaf5e967deba0bef30155dd5e04d8c19a3
Shreyaskr8 <skrravi08@gmail.com>
2024-08-10 23:01:44 +0530

    fix project loading order issue

--- src/components/storage.js
+++ src/components/storage.js
@@ -1,8 +1,16 @@
 import Project from "./project";
 import Todo from "./todo";
 
+const PROJECT_ORDER_KEY = "projectOrder";
+
 function saveProjectToLocalStorage(project) {
     localStorage.setItem(project.name, JSON.stringify(project)); //set the stringified object to its name key
+
+    const projectOrder = JSON.parse(localStorage.getItem(PROJECT_ORDER_KEY)) || [];
+    if(!projectOrder.includes(project.name)) {   //required as every update on project, this func is called.
+        projectOrder.push(project.name);
+        localStorage.setItem(PROJECT_ORDER_KEY, JSON.stringify(projectOrder));
+    }
 }
 
 function loadProjectFromLocalStorage(projectName) {
@@ -28,19 +36,20 @@ function loadProjectFromLocalStorage(projectName) {
 
 function deleteProjectFromLocalStorage(projectName) {
     localStorage.removeItem(projectName);
-}
-function updateProjectToLocalStorage(projectName, projectNewName) {
-    saveProjectToLocalStorage(project)
+
+    let projectOrder = JSON.parse(localStorage.getItem(PROJECT_ORDER_KEY)) || [];
+    projectOrder = projectOrder.filter(name => name !== projectName);
+    localStorage.setItem(PROJECT_ORDER_KEY, JSON.stringify(projectOrder));
 }
 
 function getAllProjectNames() {
-    return Object.keys(localStorage);
+    // return Object.keys(localStorage);
+    return JSON.parse(localStorage.getItem(PROJECT_ORDER_KEY)) || [];
 }
 
 export default {
     saveProjectToLocalStorage,
     loadProjectFromLocalStorage,
     deleteProjectFromLocalStorage,
-    updateProjectToLocalStorage,
     getAllProjectNames,
 };
